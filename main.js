000ca0aa1f66b83934d5286d1bb2693e1e1f5f4b
bxlt <xuelitan123@gmail.com>
2025-04-26 23:34:16 -0400

    refactor library into class

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-const myLibrary = [];
-const libList = document.getElementById('bookList');
-const addBookButton = document.getElementById('addBook');
-const bookForm = document.getElementById('bookForm');
-const closeBtn = document.getElementById('closeBtn');
-const bookInput = document.getElementById('bookDialog');
-const deleteBookBtn = document.getElementsByClassName('deleteBook');
-
 class Book{
     constructor(name, author, pages,read=false) {
         // the constructor...
@@ -21,132 +13,157 @@ class Book{
     }
 }
 
-function addBookToLibrary(name,author,pages, read) {
-  // take params, create a book then store it in the array
-  let curr = new Book(name,author,pages,read);
-  console.log(curr);
-  myLibrary.push(curr);
-    addRow(curr);
 
-  //console.log(myLibrary);
-}
+class Library{
 
-function updatePage(){
-    for(let i =0;i<myLibrary.length;i++){
-        addRow(myLibrary[i]);
-        
-        
-        console.log(myLibrary[i]);
+    constructor(){
+        this.myLibrary = [];
+        this.libList = document.getElementById('bookList');
+        this.addBookButton = document.getElementById('addBook');
+        this.bookForm = document.getElementById('bookForm');
+        this.closeBtn = document.getElementById('closeBtn');
+        this.bookInput = document.getElementById('bookDialog');
+        this.deleteBookBtn = document.getElementsByClassName('deleteBook');
+        this.bindEvents();
     }
 
-}
-
-function addRow(book){
-    let curr = libList.insertRow(-1);
-    let currName = curr.insertCell(-1);
-    let currA = curr.insertCell(-1);
-    let currP = curr.insertCell(-1);
-    let currR = curr.insertCell(-1);
-
-    let close = document.createElement('BUTTON');
-    close.setAttribute('class', 'deleteBook');
-    close.innerText='X'
-    close.addEventListener('click',()=>{
-
-        deleteFromLib(close.parentNode.parentNode.getAttribute('data-id'));
-        close.parentNode.parentNode.remove();
-    })
-
-    let willRead = document.createElement('input');
-    willRead.setAttribute('type','checkbox');
-    let checkRead = curr.insertCell(-1);
-    checkRead.appendChild(willRead);
-    willRead.addEventListener('click',function(){
-        let id = willRead.parentNode.parentNode.getAttribute('data-id');
-        updateLib(id);
-        updateHtml(id);
-    });
-
-    let deleteBtn = curr.insertCell(-1);
-    deleteBtn.appendChild(close);
-
-    curr.setAttribute('data-id', book.id,'class', 'bookRow');
-    currName.innerHTML = book.name;
-    currA.innerHTML = book.author;
-    currP.innerHTML = book.pages;
-    if(book.read){
-        currR.innerHTML = "Yes";
-    }else{
-        currR.innerHTML = "No";
-    }
-}
-
-//show form when click
-addBookButton.addEventListener("click",()=>{
-    bookInput.showModal();
-    addBookButton.style.display='none';
-})
-
-//close book form
-closeBtn.addEventListener('click', ()=>{
-    bookInput.close();
-    addBookButton.style.display='block';
-})
-
-//add book from html
-bookForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let name = document.getElementById('bName');
-    let author = document.getElementById('bAuthor');
-    let pages = document.getElementById('bPage');
-    let isRead = document.getElementById('bRead');
-    //console.log(isRead.checked)
-
-    if(name.value==""||author.value==""||pages.value==""){
-        alert("Please enter book title, author, page number");
-        return false;
-    }else{
-        addBookToLibrary(name.value, author.value, pages.value, isRead.checked);
-        //bookForm.style.display='none';
-        addBookButton.style.display='block';
-        bookForm.reset();
-        bookInput.close();
+    bindEvents() {
+        this.addBookButton.addEventListener("click", () => this.showForm());
+        this.closeBtn.addEventListener('click', () => this.closeBook());
+        this.bookForm.addEventListener('submit', (e)=> this.addBook(e));
     }
 
-})
 
-//update book property
-function updateLib(id){
-    for(var i=0;i<myLibrary.length;i++){
-        if(myLibrary[i].id===id){
-            myLibrary[i].read = true;
-            break;
-        }
-    }
-}
 
-function updateHtml(id){
+    //show form when click
+    showForm() {
     
-    for(let r of libList.rows){
-        if(r.getAttribute('data-id')===id){
-            r.cells[3].innerText = 'Yes';
-            r.cells[4].style.display ='none';
-            break;
+    this.bookInput.showModal();
+    this.addBookButton.style.display='none';    
+    }
+    
+    //close book form
+    closeBook(){
+        this.bookInput.close();
+        this.addBookButton.style.display='block';
+    }
+
+    //add book from html
+
+    addBook(e) {
+        e.preventDefault();
+        let name = document.getElementById('bName');
+        let author = document.getElementById('bAuthor');
+        let pages = document.getElementById('bPage');
+        let isRead = document.getElementById('bRead');
+        //console.log(isRead.checked)
+
+        if(name.value==""||author.value==""||pages.value==""){
+            alert("Please enter book title, author, page number");
+            return false;
+        }else{
+            this.addBookToLibrary(name.value, author.value, pages.value, isRead.checked);
+        //bookForm.style.display='none';
+        this.addBookButton.style.display='block';
+        this.bookForm.reset();
+        this.bookInput.close();
+        }
+    }
+    
+    addBookToLibrary(name,author,pages, read) {
+        // take params, create a book then store it in the array
+        let curr = new Book(name,author,pages,read);
+        //console.log(curr);
+        this.myLibrary.push(curr);
+        this.addRow(curr);
+      
+        //console.log(myLibrary);
+    }
+
+    addRow(book){
+        let curr = this.libList.insertRow(-1);
+        let currName = curr.insertCell(-1);
+        let currA = curr.insertCell(-1);
+        let currP = curr.insertCell(-1);
+        let currR = curr.insertCell(-1);
+    
+        let close = document.createElement('BUTTON');
+        close.setAttribute('class', 'deleteBook');
+        close.innerText='X'
+        close.addEventListener('click',()=>{
+            this.deleteFromLib(close.parentNode.parentNode.getAttribute('data-id'));
+            close.parentNode.parentNode.remove();
+        })
+    
+        let checkReadBox = document.createElement('input');
+        checkReadBox.setAttribute('type','checkbox');
+        let checkRead = curr.insertCell(-1);
+        checkRead.appendChild(checkReadBox);
+        checkReadBox.addEventListener('click',()=>{
+            console.log("change read status");
+            this.updateReadStatus(book.id);
+        });
+    
+        let deleteBtn = curr.insertCell(-1);
+        deleteBtn.appendChild(close);
+    
+        curr.setAttribute('data-id', book.id,'class', 'bookRow');
+        currName.innerHTML = book.name;
+        currA.innerHTML = book.author;
+        currP.innerHTML = book.pages;
+        if(book.read){
+            currR.innerHTML = "Yes";
+        }else{
+            currR.innerHTML = "No";
+        }
+    }
+
+    updateReadStatus(id){
+        this.updateLib(id);
+        this.updateHtml(id);
+    }
+    updatePage(){
+        for(let i =0;i<this.myLibrary.length;i++){
+            this.addRow(this.myLibrary[i]); 
+            console.log(this.myLibrary[i]);
+        }
+    
+    }
+
+    //update book property
+    updateLib(id){
+        for(var i=0;i<this.myLibrary.length;i++){
+            if(this.myLibrary[i].id===id){
+                this.myLibrary[i].read = true;
+                break;
+            }
         }
     }
 
 
-}
-
-//delete book from myLib
-function deleteFromLib(id){
-    //console.log(myLibrary)
-    //console.log(id);
-    for(var i=0;i<myLibrary.length;i++){
-        if(myLibrary[i].id===id){
-            myLibrary.splice(i,1);
-            break;
+    updateHtml(id){
+        for(let r of this.libList.rows){
+            if(r.getAttribute('data-id')===id){
+                r.cells[3].innerText = 'Yes';
+                r.cells[4].style.display ='none';
+                break;
+            }
         }
     }
+
+
+    //delete book from myLib
+    deleteFromLib(id){
+        //console.log(myLibrary)
+        //console.log(id);
+        for(var i=0;i<this.myLibrary.length;i++){
+            if(this.myLibrary[i].id===id){
+                this.myLibrary.splice(i,1);
+                break;
+            }
+        }
     //console.log(myLibrary)
+    }
 }
+
+const myLibrary = new Library();
